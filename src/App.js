649a60742b43c1f6d9df6d6146801aeb7916c3df
viviolamaya <viviolaputri15@gmail.com>
2021-02-02 18:07:11 +0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
       <Navbar />
+      <Footer />
     </Fragment>
   );
 }

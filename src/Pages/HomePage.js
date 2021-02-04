e5388ac26e5756595bd394be30378ebe0ca6efa7
Wahyu Ramadhan <akunwahyuarhel@gmail.com>
2021-02-04 23:36:55 +0700

    showing casting movie & review movie

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -8,76 +8,77 @@ import { Card, CardBody, CardImg, Col, Container, Row } from "reactstrap";
 import { StyledGenre, Title, GroupButton } from '../Assets/Styles/styled';
 import PaginationMovie from '../Components/Pagination';
 
-const HomePage = ({getGenreList, getMovieByGenre, getMovies, movies, genres, id}) => {
+const HomePage = ({ getGenreList, getMovieByGenre, getMovies, movies, genres, id }) => {
 
-    useEffect(() => {
-        getMovies();
-    }, [getMovies])
+  useEffect(() => {
+    getMovies();
+  }, [getMovies])
 
-    useEffect(() => {
-        getGenreList();
-    }, [getGenreList]);
+  useEffect(() => {
+    getGenreList();
+  }, [getGenreList]);
 
-    return (
+  return (
     <>
-    <style>
+      <style>
         {
           `.carousel img{
               max-width: 100%;
               height: 450px;
+              object-fit: cover;
             }`
         }
       </style>
-      <UncontrolledCarousel className='carousel' items={items} style={{width: '100%', height: '400px'}}/>
+      <UncontrolledCarousel className='carousel' items={items} style={{ width: '100%', height: '400px' }} />
       <br />
       <StyledGenre>
         <p>Browse by Category</p>
       </StyledGenre>
-        <Container >
-          {genres.length !== 0 ? (
+      <Container >
+        {genres.length !== 0 ? (
           genres.map((genre) => (
-            <GroupButton outline color="info" key={genre.id} onClick={() => getMovieByGenre(genre.id)}>{genre.name}</GroupButton>    
+            <GroupButton outline color="info" key={genre.id} onClick={() => getMovieByGenre(genre.id)}>{genre.name}</GroupButton>
           ))
-        ) : " " }
+        ) : " "}
         <p></p>
         <Row >
           {movies !== 0 ? movies.map((movie) => (
-                <Col md={3} key={movie.id} >
-                <Link
-                  to={`/detail-movie/${movie.id}`}
-                  style={{ textDecoration: "none", color: "black"}}
-                >
-                  <Card style={{marginBottom: "20px"}}>
-                    <CardImg src={`${imgUrl}${movie.poster_path}`} />
-                    <CardBody>
+            <Col md={3} key={movie.id} >
+              <Link
+                to={`/detail-movie/${movie.id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <Card style={{ marginBottom: "20px" }}>
+                  <CardImg src={`${imgUrl}${movie.poster_path}`} />
+                  <CardBody>
                     <Title>{movie.title}</Title>
                     <p>{movie.release_date}</p>
-                    </CardBody>
-                  </Card>
-                  </Link>
-              </Col>
-              )) : ""}
+                  </CardBody>
+                </Card>
+              </Link>
+            </Col>
+          )) : ""}
         </Row>
-          <PaginationMovie />
-        </Container>
-      </>
-    )
+        <PaginationMovie />
+      </Container>
+    </>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-        movies: state.homePage.movies,
-        genres: state.homePage.genres,
-        id: state.homePage.id,
-    }
+  return {
+    movies: state.homePage.movies,
+    genres: state.homePage.genres,
+    id: state.homePage.id,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-        getMovies: () => dispatch(getMovies()),
-        getGenreList: () => dispatch(getGenreList()),
-        getMovieByGenre: (id) => dispatch(getMovieByGenre(id)),
-    }
+  return {
+    getMovies: () => dispatch(getMovies()),
+    getGenreList: () => dispatch(getGenreList()),
+    getMovieByGenre: (id) => dispatch(getMovieByGenre(id)),
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
